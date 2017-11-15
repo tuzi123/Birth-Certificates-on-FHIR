245f5db5aee89ff47792c361c40e69a321b34929
Su, Xiaoming <xsu39@gatech.edu>
2017-11-14 23:44:11 -0500

    Update populateForm.js

--- project/web/populateForm.js
+++ project/web/populateForm.js
@@ -15,49 +15,57 @@ var demo = {
           type: 'none'
         }}
 
+// http://localhost:8080/fhir-app/web/index.html
 var smart = FHIR.client(demo);
 var pq = smart.patient.read(); 
 
 pq.then(function(p) {
-        var gender = p.gender;
-        var name = p.name[0];
-        var firstName = name.given
-        var LastName = name.family
-        var firstNameFill = document.getElementById("childFirst");
-        firstNameFill.value = firstName;
-        var lastNameFill = document.getElementById("childLast");
-        lastNameFill.value = LastName;
-        var genderFill = document.getElementById("childGender");
-        genderFill.value = gender;
+    var gender = p.gender;
+    var name = p.name[0];
+    var firstName = name.given
+    var LastName = name.family
+    var firstNameFill = document.getElementById("childFirst");
+    firstNameFill.value = firstName;
+    var lastNameFill = document.getElementById("childLast");
+    lastNameFill.value = LastName;
+    var genderFill = document.getElementById("childGender");
+    genderFill.value = gender;
 
-        var birth = p.birthDate;
-        var birthFill = document.getElementById("childBirth");
-        birthFill.value = birth;
+    var birth = p.birthDate;
+    var birthFill = document.getElementById("childBirth");
+    birthFill.value = birth;
 
-        if(p.address[0].use=="home"){
-            var babyAddress = p.address[0].line;
-            var addressFill = document.getElementById("homeAddress");
-            addressFill.value = babyAddress;
+    if(p.address[0].use=="home"){
+        var babyAddress = p.address[0].line;
+        var addressFill = document.getElementById("homeAddress");
+        addressFill.value = babyAddress;
 
-            var babyCity = p.address[0].city;
-            var cityFill = document.getElementById("childCity");
-            cityFill.value = babyCity;
+        var babyCity = p.address[0].city;
+        var cityFill = document.getElementById("childCity");
+        cityFill.value = babyCity;
 
-            var babyPC = p.address[0].postalCode;
-            var postalCodeFill = document.getElementById("childPostCode");
-            postalCodeFill.value = babyPC;
+        var babyPC = p.address[0].postalCode;
+        var postalCodeFill = document.getElementById("childPostCode");
+        postalCodeFill.value = babyPC;
 
-            var babyCountry = p.address[0].country;
-            var countryFill = document.getElementById("childCountry");
-            countryFill.value = babyCountry;
+        var babyCountry = p.address[0].country;
+        var countryFill = document.getElementById("childCountry");
+        countryFill.value = babyCountry;
 
-            var babyState = p.address[0].state;
-            var stateFill = document.getElementById("childState");
-            stateFill.value = babyState;
-        }
+        var babyState = p.address[0].state;
+        var stateFill = document.getElementById("childState");
+        stateFill.value = babyState;
+    }
+
+    document.getElementById("DeceasedNo").checked = true;
+    document.getElementById("DeceasedYes").checked = false;
+    var deceased = p.deceased;
+    if (deceased){
+        document.getElementById("DeceasedYes").checked = true;
+        document.getElementById("DeceasedNo").checked = false;}
 
 
-        smart.patient.api.search({count:10, type: "Immunization", query: {patient: demo.patientId},
+    smart.patient.api.search({count:10, type: "Immunization", query: {patient: demo.patientId},
         }).then(function(r){
               for (re of r.data.entry) {
                 var rx = re.resource; 
@@ -75,7 +83,7 @@ pq.then(function(p) {
 // window.onload = function() {
 //     document.getElementById("chkNoAbnormalConditions").checked = true;
 // };
-smart.patient.api.search({count:10, type: "Procedure", query: {patient: demo.patientId},
+    smart.patient.api.search({count:10, type: "Procedure", query: {patient: demo.patientId},
         }).then(function(r){
               for (re of r.data.entry) {
               document.getElementById("chkNoAbnormalConditions").checked = true;
@@ -92,7 +100,7 @@ smart.patient.api.search({count:10, type: "Procedure", query: {patient: demo.pat
             };
         });
 
-smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
+    smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
         }).then(function(r){
               for (re of r.data.entry) {
               var rx = re.resource; 
@@ -103,7 +111,7 @@ smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.p
             };
         });
 
-smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
+    smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
         }).then(function(r){
               for (re of r.data.entry) {
               var rx = re.resource; 
@@ -114,7 +122,7 @@ smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.p
             };
         });
 
-smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
+    smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.patientId},
         }).then(function(r){
               for (re of r.data.entry) {
               var rx = re.resource; 
@@ -127,8 +135,9 @@ smart.patient.api.search({count:20, type: "Observation", query: {patient: demo.p
 
 
       }, function (error) {
+    // console.error("uh oh: ", error); 
     alert("Cannot find the baby with the given ID, please double check!");
     // document.getElementById("submitForm").disabled = true;
-    location.href = 'search.html';
+    location.href = 'search.php';
 });
 
