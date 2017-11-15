dfe4bf01710686ec1a94ca719c6287b8d16b3c1c
Su, Xiaoming <xsu39@gatech.edu>
2017-11-14 23:45:39 -0500

    Update and rename register.html to register.php

--- project/web/register.php
+++ project/web/register.php
@@ -23,13 +23,13 @@
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li ><a href="index.html">Home</a></li>
-        <li ><a href="about.html">About</a></li>
+        <li ><a href="index.php">Home</a></li>
+        <li ><a href="about.php">About</a></li>
 <!--         <li><a href="#contact">Contact</a></li> -->
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a class="active" href="register.html">Register  <i class="fa fa-user-plus"></i></a></li>
-        <li><a href="login.html">Login  <i class="fa fa-user"></i></a></li>
+        <li><a href="login.php">Login  <i class="fa fa-user"></i></a></li>
       </ul>
     </div>
   </div>
@@ -44,7 +44,7 @@
           <div class="panel-body">
             <div class="row">
               <div class="col-lg-12">
-                <form id="register-form" action="#" method="post" role="form" style="display: block;">
+                <form id="register-form" action="register.php" method="post" role="form" style="display: block;">
                   <div class="form-group">
                     <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
                   </div>
@@ -60,8 +60,7 @@
                   <div class="form-group">
                     <div class="row">
                       <div class="col-sm-6 col-sm-offset-3">
-                        <input type="submit" name="register-submit" href="login.html" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
-                        
+                        <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
                       </div>
                     </div>
                   </div>
@@ -76,6 +75,64 @@
 
  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<?php 
+if(isset($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["email"])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	$connect = new mysqli('localhost','root',"") or die(myspqli_error());
+	$db = mysqli_select_db($connect,'registration') or die("DB error");
+	$query = mysqli_query($connect, "SELECT * FROM account WHERE username='".$username."'");
+	$numrows = mysqli_num_rows($query);
+	if ($numrows ==0)
+	{
+		$sql = "INSERT INTO account(username,password,email) VALUES('$username','$password','$email')";
+		$result = mysqli_query($connect,$sql);
+		if($result)
+		{   header("Location:login.html");
+			?>
+            <div class="container"> 
+            	<div class="row">
+            		<div class="content">
+            	            <h2>Your account was created successfully!</h2>
+            	        </div>
+            	</div>
+            </div>
+			<?php
+		}
+		else{
+			?>
+            <div class="container"> 
+            	<div class="row">
+            		<div class="content">
+            	            <h2>Your account was not created: an error occured</h2>
+            	        </div>
+            	</div>
+            </div>
+			<?php
+		}
+	}
+	else{
+		?>
+            <div class="container">
+                <div class="row"> 
+                	<div class="content">
+            	             <h2>The username already exists, please choose another one.</h2>
+            	        </div>
+            	</div>
+            </div>
+			<?php
+	}
+
+} 
+else
+    {
+	   ?>
+	   <script type="text/javascript">alert("All fields are required");</script>
+	   <?php 
+    }
+?>
 
 </body>
 </html>
